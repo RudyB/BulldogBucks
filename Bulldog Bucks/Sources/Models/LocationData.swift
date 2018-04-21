bb19b80ecbd2418a0feab6928d2bce6fcd52e416
Rudy Bermúdez <hello@rudybermudez.io>
2018-04-20 22:08:51 -0700

    Add missing category to location.json & add model

---
+++ Bulldog Bucks/Sources/Models/LocationData.swift
@@ -0,0 +1,52 @@
+//
+//  LocationData.swift
+//  Bulldog Bucks
+//
+//  Created by Rudy Bermudez on 4/20/18.
+//
+
+import Foundation
+
+
+struct Hours: Decodable {
+    let monday: String
+    let tuesday: String
+    let wednesday: String
+    let thursday: String
+    let friday: String
+    let saturday: String
+    let sunday: String
+}
+
+struct Location: Decodable {
+    let address: String
+    let lat: Double
+    let long: Double
+}
+
+enum LocationType: String, Decodable {
+    case shopping
+    case food
+}
+
+struct LocationData: Decodable {
+    let name: String
+    let description: String
+    let hours: Hours
+    let phone: String
+    let location: Location
+    let url: URL?
+    let menuUrl: URL?
+    let category: LocationType
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case description = "desc"
+        case hours
+        case phone
+        case url
+        case menuUrl = "menu-url"
+        case category
+    }
+}
+
